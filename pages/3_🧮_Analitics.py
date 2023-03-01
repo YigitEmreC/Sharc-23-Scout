31bdf081d08f910d3c350df12bdd5eed18cb24cb
YigitEmreC <87824693+YigitEmreC@users.noreply.github.com>
2023-03-01 09:01:17 +0300

    new analitics system

--- pages/3_🧮_Analitics.py
+++ pages/3_🧮_Analitics.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-
+import altair as alt
 
 
 st.set_page_config(
@@ -43,9 +43,29 @@ sheet = client.open_by_url(sheet_url).sheet1
 # Read the data from the Google Sheet into a Pandas DataFrame
 data = pd.DataFrame(sheet.get_all_records())
 
-# Create the Streamlit app
+# Create a search bar for filtering the data
+search = st.sidebar.text_input('Search')
+if search:
+    data = data[data.astype(str).apply(lambda x: x.str.contains(search, case=False)).any(axis=1)]
 
-st.title('Scouting')
+# Create a dropdown menu for selecting a variable to sort the data by
+sort_variable = st.sidebar.selectbox('Sort By', data.columns)
 
-    # Display the data in a table
+# Create a checkbox for selecting the sort order
+sort_order = st.sidebar.checkbox('Descending', value=True)
+
+# Sort the data based on the selected variable and sort order
+data = data.sort_values(by=sort_variable, ascending=not sort_order)
+
+# Display the sorted and filtered data in a table
 st.write(data)
+
+# Create a chart based on the selected data
+selected_data = st.multiselect('Select Data', data[sort_variable])
+if selected_data:
+    chart_data = data[data[sort_variable].isin(selected_data)]
+    chart = alt.Chart(chart_data).mark_bar().encode(
+        alt.X(sort_variable),
+        alt.Y('count()')
+    ).interactive()
+    st.altair_chart(chart, use_container_width=True)
