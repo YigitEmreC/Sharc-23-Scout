63b037e4eb902ca1de1b9d821b2f591f3ec52842
yiğit <yigitemreculcuoglu@mail.com>
2023-03-22 21:29:39 +0300

    Update 4_🖼️_Sketchboard.py

--- pages/4_🖼️_Sketchboard.py
+++ pages/4_🖼️_Sketchboard.py
@@ -2,9 +2,6 @@ import pandas as pd
 from PIL import Image
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
-import os
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
 
 hide_st_style = """
             <style>
@@ -17,26 +14,23 @@ hide_st_style = """
 
 
 st.set_page_config(
-
-page_title = "Sketch",
-page_icon ="https://media.licdn.com/dms/image/C4E03AQH4UTTZc2oWaQ/profile-displayphoto-shrink_800_800/0/1570104233605?e=2147483647&v=beta&t=qSvofj0Q9GdBP2StB4aV0EEkqL-iUzZ30TE7G2Lm3DE",
+    page_title = "Sketch",
+    page_icon ="https://media.licdn.com/dms/image/C4E03AQH4UTTZc2oWaQ/profile-displayphoto-shrink_800_800/0/1570104233605?e=2147483647&v=beta&t=qSvofj0Q9GdBP2StB4aV0EEkqL-iUzZ30TE7G2Lm3DE",
 )
 
 hide_st_style = """
             <style>
             #MainMenu {visibility: hidden;}
-          footer {visibility: hidden;}
-         header {visibility: hidden;}
-          </style>
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
          """
 
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 st.sidebar.image("https://media.discordapp.net/attachments/1078818849182457906/1080141834833113189/QyLctghW_400x400-removebg-preview.png")
 
-
 st.markdown(':blue[**Simple Sketch App for Charged UP Field**]')
-
 
 drawing_mode = st.sidebar.selectbox(
     "Drawing tool:", ("point", "freedraw", "line", "rect", "circle", "transform")
@@ -46,21 +40,18 @@ stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
 if drawing_mode == 'point':
     point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
 stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-bg_image = Image.open(os.path.join(dir_path, 'pages', 'field.png'))
+bg_image = ('./pages/field.png')
 
 realtime_update = st.sidebar.checkbox("Update in realtime", True)
 
-    
 canvas_result = st_canvas(
-    fill_color="rgba(255, 165, 0, 0.3)",
+    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
     stroke_width=stroke_width,
     stroke_color=stroke_color,
-    background_image=bg_image,
+    background_image=Image.open(bg_image),
     update_streamlit=realtime_update,
     height=400,
     drawing_mode=drawing_mode,
     point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
     key="canvas",
 )
-
-
