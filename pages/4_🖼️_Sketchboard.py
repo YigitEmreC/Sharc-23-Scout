a29d05aa53e7e4ea96fe74e1226b212b94867db5
yiğit <yigitemreculcuoglu@mail.com>
2023-03-22 21:26:23 +0300

    Update 4_🖼️_Sketchboard.py

--- pages/4_🖼️_Sketchboard.py
+++ pages/4_🖼️_Sketchboard.py
@@ -2,6 +2,9 @@ import pandas as pd
 from PIL import Image
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 hide_st_style = """
             <style>
@@ -43,17 +46,16 @@ stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
 if drawing_mode == 'point':
     point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
 stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-bg_image = ('./pages/field.png')
+bg_image = Image.open(os.path.join(dir_path, 'pages', 'field.png'))
 
 realtime_update = st.sidebar.checkbox("Update in realtime", True)
 
     
-
 canvas_result = st_canvas(
-    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+    fill_color="rgba(255, 165, 0, 0.3)",
     stroke_width=stroke_width,
     stroke_color=stroke_color,
-    background_image=Image.open(bg_image),
+    background_image=bg_image,
     update_streamlit=realtime_update,
     height=400,
     drawing_mode=drawing_mode,
