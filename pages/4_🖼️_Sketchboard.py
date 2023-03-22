2933817735d4a166de821e8f574abdef822a54c4
yiğit <yigitemreculcuoglu@mail.com>
2023-03-22 22:13:00 +0300

    Update 4_🖼️_Sketchboard.py

--- pages/4_🖼️_Sketchboard.py
+++ pages/4_🖼️_Sketchboard.py
@@ -2,8 +2,6 @@ import pandas as pd
 from PIL import Image
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
-import os
-
 
 hide_st_style = """
             <style>
@@ -45,21 +43,7 @@ stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
 if drawing_mode == 'point':
     point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
 stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-
-bg_image = None
-
-try:
-    for root, dirs, files in os.walk("/"):
-        for file in files:
-            if file == "field.jpg":
-                # Assign the file path to the variable
-                bg_image = os.path.join(root, file)
-                break
-
-        if bg_image:
-            break
-except FileNotFoundError:
-    pass
+bg_image = ('./pages/field.png')
 
 realtime_update = st.sidebar.checkbox("Update in realtime", True)
 
@@ -69,10 +53,12 @@ canvas_result = st_canvas(
     fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
     stroke_width=stroke_width,
     stroke_color=stroke_color,
-    background_image=Image.open(bg_image) if bg_image else None,
+    background_image=Image.open(bg_image),
     update_streamlit=realtime_update,
     height=400,
     drawing_mode=drawing_mode,
     point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
     key="canvas",
 )
+
+
