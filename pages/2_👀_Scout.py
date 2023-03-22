bb4b1cdda9686f3834f047f6c2a47257f20c5482
YigitEmreC <87824693+YigitEmreC@users.noreply.github.com>
2023-03-22 14:03:45 +0300

    Update 2_👀_Scout.py

--- pages/2_👀_Scout.py
+++ pages/2_👀_Scout.py
@@ -266,11 +266,11 @@ with st.expander("Results"):
      def manualParkCalculator(parkState):
          parkPoints = 0
              if parkState == "Parked":
-                parkPoints += 2            
+                 parkPoints += 2            
              elif parkState == "Docked":
-                parkPoints += 6
+                 parkPoints += 6
              elif parkState == "Engaged":
-                parkPoints += 10
+                 parkPoints += 10
             
          return parkPoints     
      
