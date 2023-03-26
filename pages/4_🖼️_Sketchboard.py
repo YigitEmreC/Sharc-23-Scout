cf58350ac540965c0b13ae5076aac9c27440325b
yiğit <yigitemreculcuoglu@mail.com>
2023-03-26 20:24:27 +0300

    Update 4_🖼️_Sketchboard.py

--- pages/4_🖼️_Sketchboard.py
+++ pages/4_🖼️_Sketchboard.py
@@ -1,6 +1,5 @@
-import requests
+import pandas as pd
 from PIL import Image
-import io
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 
@@ -13,29 +12,25 @@ hide_st_style = """
             """
 
 st.set_page_config(
-    page_title = "Sketch",
-    page_icon ="https://media.licdn.com/dms/image/C4E03AQH4UTTZc2oWaQ/profile-displayphoto-shrink_800_800/0/1570104233605?e=2147483647&v=beta&t=qSvofj0Q9GdBP2StB4aV0EEkqL-iUzZ30TE7G2Lm3DE",
+    page_title="Sketch",
+    page_icon="https://media.licdn.com/dms/image/C4E03AQH4UTTZc2oWaQ/profile-displayphoto-shrink_800_800/0/1570104233605?e=2147483647&v=beta&t=qSvofj0Q9GdBP2StB4aV0EEkqL-iUzZ30TE7G2Lm3DE",
 )
 
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 st.sidebar.image("https://media.discordapp.net/attachments/1078818849182457906/1080141834833113189/QyLctghW_400x400-removebg-preview.png")
 
-st.markdown(':blue[**Simple Sketch App for Charged UP Field**]')
+st.markdown(":blue[**Simple Sketch App for Charged UP Field**]")
 
 drawing_mode = st.sidebar.selectbox(
     "Drawing tool:", ("point", "freedraw", "line", "rect", "circle", "transform")
 )
 
 stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
-
-if drawing_mode == 'point':
+if drawing_mode == "point":
     point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
-
 stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-
-response = requests.get('https://cdn.discordapp.com/attachments/738557702330122283/1078717809225379860/Screenshot_7.png')
-bg_image = Image.open(io.BytesIO(response.content)).convert('RGB')
+bg_image_url = "https://cdn.discordapp.com/attachments/738557702330122283/1078717809225379860/Screenshot_7.png"
 
 realtime_update = st.sidebar.checkbox("Update in realtime", True)
 
@@ -43,10 +38,10 @@ canvas_result = st_canvas(
     fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
     stroke_width=stroke_width,
     stroke_color=stroke_color,
-    background_image=bg_image,
+    background_image=Image.open(bg_image_url),
     update_streamlit=realtime_update,
     height=400,
     drawing_mode=drawing_mode,
-    point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
+    point_display_radius=point_display_radius if drawing_mode == "point" else 0,
     key="canvas",
 )
