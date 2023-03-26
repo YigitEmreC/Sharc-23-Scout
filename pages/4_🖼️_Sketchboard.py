3670019b0dff39e133bc29146ac42f7a48557aae
yiğit <yigitemreculcuoglu@mail.com>
2023-03-26 14:33:08 +0300

    Update 4_🖼️_Sketchboard.py

--- pages/4_🖼️_Sketchboard.py
+++ pages/4_🖼️_Sketchboard.py
@@ -48,14 +48,13 @@ bg_image = ('./pages/field.png')
 realtime_update = st.sidebar.checkbox("Update in realtime", True)
 
 canvas_result = st_canvas(
-       
+
     stroke_width=stroke_width,
     stroke_color=stroke_color,
     background_image=Image.open(bg_image),
     update_streamlit=realtime_update,
     height=400,
     drawing_mode=drawing_mode,
-    point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
     key="canvas",
 )
 
