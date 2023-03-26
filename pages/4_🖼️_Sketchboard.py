4f447ab927166f4f74f33b544a743923682f05bb
yiğit <yigitemreculcuoglu@mail.com>
2023-03-26 14:35:13 +0300

    Update 4_🖼️_Sketchboard.py

--- pages/4_🖼️_Sketchboard.py
+++ pages/4_🖼️_Sketchboard.py
@@ -1,62 +1,38 @@
-import pandas as pd
-from PIL import Image
 import streamlit as st
-from streamlit_drawable_canvas import st_canvas
+import numpy as np
+from PIL import Image
+import io
+import cv2
 
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
+# Define function to convert image to RGBA format
+def image_to_rgba(image):
+    if image.mode == 'RGB':
+        r, g, b = image.split()
+        a = Image.new('L', image.size, 255)
+        image = Image.merge('RGBA', (r, g, b, a))
+    return image
 
+# Load image from URL
+url = 'https://cdn.discordapp.com/attachments/738557702330122283/1078717809225379860/Screenshot_7.png'
+response = requests.get(url)
+image = Image.open(io.BytesIO(response.content))
+image = image_to_rgba(image)
 
+# Create a canvas to draw on
+canvas_size = st.sidebar.slider('Canvas size', 200, 800, 400)
+canvas = st_canvas(
+    fill_color='rgba(255, 255, 255, 0.3)',  # Set background color to transparent
+    stroke_width=5,
+    stroke_color='red',
+    background_image=image,
+    width=canvas_size,
+    height=canvas_size,
+    drawing_mode='freedraw',
+    key='canvas')
 
-st.set_page_config(
+# Convert the canvas to an image
+if canvas.image_data is not None:
+    img = Image.fromarray(canvas.image_data.astype('uint8'), 'RGBA')
+    img.save('drawing.png')
+    st.image(img, caption='Your drawing', use_column_width=True)
 
-page_title = "Sketch",
-page_icon ="https://media.licdn.com/dms/image/C4E03AQH4UTTZc2oWaQ/profile-displayphoto-shrink_800_800/0/1570104233605?e=2147483647&v=beta&t=qSvofj0Q9GdBP2StB4aV0EEkqL-iUzZ30TE7G2Lm3DE",
-)
-
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-          footer {visibility: hidden;}
-         header {visibility: hidden;}
-          </style>
-         """
-
-st.markdown(hide_st_style, unsafe_allow_html=True)
-
-st.sidebar.image("https://media.discordapp.net/attachments/1078818849182457906/1080141834833113189/QyLctghW_400x400-removebg-preview.png")
-
-
-st.markdown(':blue[**Simple Sketch App for Charged UP Field**]')
-
-
-drawing_mode = st.sidebar.selectbox(
-    "Drawing tool:", ("point", "freedraw", "line", "rect", "circle", "transform")
-)
-
-stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
-if drawing_mode == 'point':
-    point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
-stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-bg_image = ('./pages/field.png')
-
-realtime_update = st.sidebar.checkbox("Update in realtime", True)
-
-canvas_result = st_canvas(
-
-    stroke_width=stroke_width,
-    stroke_color=stroke_color,
-    background_image=Image.open(bg_image),
-    update_streamlit=realtime_update,
-    height=400,
-    drawing_mode=drawing_mode,
-    key="canvas",
-)
-
-
- 
