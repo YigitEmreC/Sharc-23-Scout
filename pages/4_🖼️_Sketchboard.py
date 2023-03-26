091d8dd594408cb59a052283d668e25669227507
yiğit <yigitemreculcuoglu@mail.com>
2023-03-26 20:11:32 +0300

    Update 4_🖼️_Sketchboard.py

--- pages/4_🖼️_Sketchboard.py
+++ pages/4_🖼️_Sketchboard.py
@@ -43,7 +43,7 @@ stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
 if drawing_mode == 'point':
     point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
 stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-bg_image = Image.open('./pages/field.png')
+bg_image = Image.open('./pages/field.png').convert('RGB')
 realtime_update = st.sidebar.checkbox("Update in realtime", True)
 
     
