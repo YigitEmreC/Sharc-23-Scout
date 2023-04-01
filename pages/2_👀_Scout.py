4d2513f16cf4a439756db277d5a6deee8526669d
Yiğit <87824693+YigitEmreC@users.noreply.github.com>
2023-04-01 16:33:32 +0300

    Update 2_👀_Scout.py

--- pages/2_👀_Scout.py
+++ pages/2_👀_Scout.py
@@ -322,18 +322,18 @@ async def main():
     spreadsheet = await agc.open('scouting')
     sheet = await spreadsheet.get_worksheet(1)  # assume the sheet is the first one
 
-sync def writeSheet429(row):
-   while True:
-       try:
-               await sheet.append_row(row)
-        break  # Exit the loop if the process is successful
+async def writeSheet429(row):
+    while True:
+        try:
+            await sheet.append_row(row)
+            break  # Exit the loop if the process is successful
         except gspread.exceptions.APIError as e:
-           if e.response.status_code == 429:
-                    # If there is a 429 error, wait for a certain amount of time before retrying
-               await asyncio.sleep(5)  # Put the app to sleep for 60 seconds since the quota resets per minute
+            if e.response.status_code == 429:
+                # If there is a 429 error, wait for a certain amount of time before retrying
+                await asyncio.sleep(5)  # Put the app to sleep for 5 seconds since the quota resets per minute
             else:
-                    # If it's not a 429 error, raise the exception
-               raise e
+                # If it's not a 429 error, raise the exception
+                raise e
 
  
 asyncio.run(main())
